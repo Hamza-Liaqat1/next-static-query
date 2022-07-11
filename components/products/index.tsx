2754595325802ab77614c7fcc87bf787060b94cf
Igor <thelastwaymusic@gmail.com>
2022-07-11 20:32:51 +0300

    Fake products

--- components/products/index.tsx
+++ components/products/index.tsx
@@ -3,7 +3,18 @@ import Pagination from "../ui/pagination";
 const Products = () => {
   return (
     <div className="flex flex-col items-center gap-y-4">
-      <div>Products</div>
+      <div className="flex w-full p-4">
+        <div className="w-full grid gap-4 grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+          {Array(12)
+            .fill(0)
+            .map((_, i) => (
+              <div key={i} className="productCard">
+                Product {i + 1}
+              </div>
+            ))}
+        </div>
+      </div>
+
       <Pagination queryKey="page" pages={5} />
     </div>
   );
